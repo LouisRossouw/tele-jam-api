90acc7d539158696217a8c5761502535c288b5b6
Louis <lourou99@gmail.com>
2025-12-04 23:43:56 +0200

    Added notifications condition and output a manifest details of the last send notification

--- lib/notification.py
+++ lib/notification.py
@@ -1,38 +1,58 @@
 import os
+import json
 import telebot
+
+from lib.utils import write_to_json
 
 from dotenv import load_dotenv
 load_dotenv()
 
-BOT_TOKEN = os.getenv("telegram_token")
 TELEBOT_ADMIN_ALIAS = os.getenv("telegram_chat_ID")
 
-# TODO;
 
-
-class Notification():
+class Notification:
     """ Basic Notification class for Telebot. """
 
-    def __init__(self):
+    def __init__(self, settings):
         """ initialize baby. """
 
-        # TODO; Get this from the config
-        self.notifications = True
+        self.settings = settings
+        self.manifest = settings.manifest
 
-        self.bot = telebot.TeleBot(BOT_TOKEN)
         self.ADMIN_ID = TELEBOT_ADMIN_ALIAS
 
-    def send_notification(self, text):
-        """ Send notification to chat ID only. 
-        :param text: string """
+    def send_notification(self, text, BOT_TOKEN):
+        """ Send notification to chat ID only. """
 
-        if self.notifications == True:
+        bot = telebot.TeleBot(BOT_TOKEN)
+        notifications = self.settings.get_setting("notifications")
+
+        if notifications == True:
             try:
-                self.bot.send_message(chat_id=self.ADMIN_ID,
-                                      allow_sending_without_reply=True, text=str(text))
+                res = bot.send_message(
+                    chat_id=self.ADMIN_ID,
+                    allow_sending_without_reply=True,
+                    text=str(text)
+                )
+
+                if res:
+                    payload = res.json
+
+                    # Write out the response from the last notification sent.
+                    if isinstance(payload, str):
+                        payload = json.loads(payload)
+                        write_to_json(self.manifest, payload)
+
+                return True
+
             except Exception as e:
                 print('TeleBot error?!')
                 print(e)
+
+                return False
+
+        else:
+            print('Notifications have been turned off.')
 
 
 if __name__ == "__main__":
