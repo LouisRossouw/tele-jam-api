5a8ea92cb9107fdfdc7ff746399b782b8d3981dc
Louis <lourou99@gmail.com>
2025-12-04 23:45:01 +0200

    Updated the api to get the requested bot by name, and send out a notification to that bot.

--- api/server.py
+++ api/server.py
@@ -1,33 +1,49 @@
+from typing import List
 from fastapi import FastAPI, status
 from fastapi.responses import JSONResponse
 
+from lib.format_telegram import build_txt
+from lib.bot import Bot
+
+from dotenv import load_dotenv
+load_dotenv()
+
 
 class TelegramAPI:
-    def __init__(self, settings):
+    def __init__(self, settings, notification):
         self.settings = settings
+        self.notification = notification
 
         self.app = FastAPI()
         self.routes()
 
     def routes(self):
-        @self.app.get("/notify")
-        def notify():
+        @self.app.get("/notify/bot/{bot_name}")
+        def notify(bot_name: str, text: List[str]):
 
-            # TODO; Send a notification via telegram.
+            bot = Bot(bot_name)
+            formatted_txt = build_txt(text)
+
+            has_sent = False
+
+            if bot.active:
+                has_sent = self.notification.send_notification(
+                    formatted_txt, bot.token)
+
+            context = {
+                "ok": True if bot.active and has_sent else False,
+                "bot_active": bot.active,
+                "bot": bot.label,
+            }
 
             return JSONResponse(
-                content={"ok": True},
+                content=context,
                 status_code=status.HTTP_200_OK
             )
 
         @self.app.get("/config")
         def config():
             return self.settings.get_config()
-
-        # TODO:
-        # @self.app.put("/config")
-        # def update_config():
-        #     return
 
     def run(self, host="0.0.0.0", port=5001):
         import uvicorn
